0a0f7a4551aed60eb5b64572caac198486f8954c
HyunCafe <eparksuncafe@gmail.com>
2023-03-02 21:51:55 -0600

    Add: Started pseudo code on how I want to customize this project

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+
+let myLibrary = [];
+
+function resources() {
+  // the constructor...
+}
+
+function addResourceToLibrary() {
+  // do stuff here
+}
+
+
+// Will Create a library in which you can sort and  add, delete, rate, percentage completed, Date Started, Date Completed, Link, Type, notes 
+
+
+// Sort Option For Different Categories
