5e20888de3b035ad6635dccbdfd9131a9f4f7f2c
HyunCafe <eparksuncafe@gmail.com>
2023-03-03 21:05:51 -0600

    Refactor: Refactored to objects and added color code option for rating

--- script.js
+++ script.js
@@ -1,26 +1,28 @@
 "use strict";
 
-const range = document.querySelector(".main__range");
-const progressBar = document.querySelector(".main__progress");
-
+// Variables
+const rating = this.rating;
+let color;
 let myLibrary = [];
 
 // Define enum for constant values
 const STATUS = {
   PENDING: "pending",
   IN_PROGRESS: "in progress",
+  HIATUS: "hiatus",
   COMPLETED: "completed",
 };
 
 // Define resourceList object
 const resourceList = {
   resource: "",
-  type: "",
+  category: "",
   status: STATUS.PENDING,
   completion: 0,
   startDate: "",
   finishDate: "",
   notes: "",
+  rating: 0,
   updateStatus: function (newStatus) {
     if (Object.values(STATUS).includes(newStatus)) {
       this.status = newStatus;
@@ -28,29 +30,43 @@ const resourceList = {
       throw new Error("Invalid status");
     }
   },
-  getCompletionProgressBar: function () {
-    const percentage = this.completion;
+  handleRatingChange: function (event) {
+    const newRating = parseInt(event.target.value);
+    if (isNaN(newRating) || newRating < 1 || newRating > 10) {
+      throw new Error("Invalid rating");
+    }
+    this.rating = newRating;
+  },
+  getRatingInput: function() {
     let color;
-    if (percentage >= 70) {
+    if (this.rating >= 7) {
       color = "green";
-    } else if (percentage >= 20) {
+    } else if (this.rating >= 2) {
       color = "blue";
     } else {
       color = "yellow";
     }
     return (
-      <div
-        class="progress-bar"
-        style="width: ${percentage}%; background-color: ${color};"
-      ></div>
+      <div class="rating-bar">
+        <input 
+          type="number" 
+          min="1" 
+          max="10" 
+          step="1" 
+          value={this.rating} 
+          onChange={this.handleRatingChange} 
+          style={{ backgroundColor: color }}
+        />
+      </div>
     );
   },
 };
 
+
 // Example usage
 const resource1 = Object.create(resourceList);
 resource1.resource = "JavaScript";
-resource1.type = "Tutorial";
+resource1.category = "Tutorial";
 resource1.status = STATUS.IN_PROGRESS;
 resource1.completion = 50;
 resource1.startDate = "2023-02-28";
