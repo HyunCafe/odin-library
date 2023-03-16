9560dc647e262e72d83eedb61ea502b8a8cb1ec1
HyunCafe <eparksuncafe@gmail.com>
2023-03-15 19:01:14 -0500

    Refactor: Refactoring modules into DOMManager

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 let myLibrary = [];
 
-// Factory Function for Creating Resources
+// Class for Creating Resources
 class Resource {
   constructor(resource, category, status, startDate, endDate, notes, rating) {
     this.resource = resource;
@@ -19,6 +19,7 @@ class Resource {
 const tableContent = document.getElementsByClassName("resource-list__body")[0];
 const tableHeaderButtons = document.querySelectorAll("th button");
 
+// Class for creating rows in the table
 class ResourceRow {
   constructor(resource, index) {
     const createResourceRow = document.createElement("tr");
@@ -43,6 +44,7 @@ class ResourceRow {
 
       switch (propName) {
         case "notes":
+          // If notes are longer than 100 characters, truncate them and add a "more" link
           if (resource[propName].length > 100) {
             const truncatedNotes = resource[propName].slice(0, 45) + "...";
             const truncatedNotesElem = document.createElement("span");
@@ -95,187 +97,10 @@ class ResourceRow {
   }
 }
 
-// Get data and save to variable for referencing
-const getTableContent = (data) => {
-  data.forEach((resource, index) => {
-    const newRow = createNewRow(resource, index);
-    tableContent.append(newRow);
-  });
-};
-
-//   Sorts by Column
-const sortData = (data, param, direction = "asc") => {
-  tableContent.textContent = "";
-  const sortedData = [...data].sort((a, b) => {
-    let aValue, bValue;
-    switch (param) {
-      case "resource-rating":
-        aValue = Number(a.rating);
-        bValue = Number(b.rating);
-        break;
-      case "resource-name":
-        aValue = a.resource.toLowerCase();
-        bValue = b.resource.toLowerCase();
-        break;
-      case "resource-category":
-        aValue = a.category.toLowerCase();
-        bValue = b.category.toLowerCase();
-        break;
-      case "resource-status":
-        aValue = a.status.toLowerCase();
-        bValue = b.status.toLowerCase();
-        break;
-      case "resource-start-date":
-        aValue = new Date(a.startDate);
-        bValue = new Date(b.startDate);
-        break;
-      case "resource-end-date":
-        aValue = new Date(a.endDate);
-        bValue = new Date(b.endDate);
-        break;
-      case "resource-notes":
-        aValue = a.notes.toLowerCase();
-        bValue = b.notes.toLowerCase();
-        break;
-      case "resource-options":
-        aValue = null;
-        bValue = null;
-        break;
-      default:
-        aValue = null;
-        bValue = null;
-        break;
-    }
-    if (aValue === null || bValue === null) return 0;
-    return direction === "asc"
-      ? aValue < bValue
-        ? -1
-        : aValue > bValue
-        ? 1
-        : 0
-      : bValue < aValue
-      ? -1
-      : bValue > aValue
-      ? 1
-      : 0;
-  });
-  getTableContent(sortedData);
-};
-
-// Reset other sorts except column clicked
-const resetButtons = (event) => {
-  [...tableHeaderButtons].map((button) => {
-    if (button !== event.target) {
-      button.removeAttribute("data-dir");
-    }
-  });
-};
-
-window.addEventListener("load", () => {
-  [...tableHeaderButtons].map((button) => {
-    button.addEventListener("click", (e) => {
-      resetButtons(e);
-      if (e.target.getAttribute("data-dir") == "desc") {
-        sortData(myLibrary, e.target.id, "desc");
-        e.target.setAttribute("data-dir", "asc");
-      } else {
-        sortData(myLibrary, e.target.id, "asc");
-        e.target.setAttribute("data-dir", "desc");
-      }
-    });
-  });
-});
-
-// Load library from local storage on page load
-window.addEventListener("load", () => {
-  const storedLibrary = localStorage.getItem("myLibrary");
-  if (storedLibrary) {
-    myLibrary = JSON.parse(storedLibrary);
-    myLibrary.forEach((resource, index) => {
-      const newRow = createNewRow(resource, index);
-      tableContent.append(newRow);
-    });
-  } else {
-    myLibrary = [];
-  }
-});
-
 // Save library to local storage whenever it changes
 const saveLibraryToLocalStorage = () => {
   localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
 };
-
-// Add Event listener for perform the new submission for row creation
-const submitButton = document.querySelector('button[type="submit"]');
-
-submitButton.addEventListener("click", (e) => {
-  e.preventDefault();
-
-  // get form values
-  const resource = document.getElementById("resource-input").value;
-  const category = document.getElementById("category-input").value;
-  const status = document.getElementById("status-input").value;
-  const startDate = document.getElementById("start-date-input").value;
-  const endDate = document.getElementById("end-date-input").value;
-  const notes = document.getElementById("notes-input").value;
-  const rating = document.getElementById("rating-input").value;
-
-  // create Resource object
-  const newResource = new Resource(
-    resource,
-    category,
-    status,
-    startDate,
-    endDate,
-    notes,
-    rating
-  );
-  // add Resource object to myLibrary array and update display
-  myLibrary.push(newResource);
-  saveLibraryToLocalStorage();
-  const newRow = createNewRow(newResource, myLibrary.length - 1);
-  tableContent.append(newRow);
-
-  // reset form values
-  document.getElementById("resource-input").value = "";
-  document.getElementById("category-input").value = "";
-  document.getElementById("status-input").value = "Pending";
-  document.getElementById("start-date-input").value = "";
-  document.getElementById("end-date-input").value = "";
-  document.getElementById("notes-input").value = "";
-  document.getElementById("rating-input").value = "";
-});
-
-// Toggle Resource form show, blur rest
-function addResourceFormShow(event) {
-  const addResourceBtn = document.querySelector(".add-resource-btn");
-  const resourceForm = document.querySelector(".resource-form");
-  const resourceSection = document.querySelector(".resource-section");
-
-  addResourceBtn.addEventListener("click", () => {
-    if (window.getComputedStyle(resourceForm).display === "none") {
-      resourceForm.style.display = "block";
-      resourceSection.style.filter = "blur(2px)";
-      document.body.style.transition = "all 0.3s ease-in-out";
-    } else {
-      resourceForm.style.display = "none";
-      resourceSection.style.filter = "";
-      document.body.style.transition = "all 0.3s ease-in-out";
-    }
-    // ability to hide display if click anywhere on body but resource form
-    document.addEventListener("click", (event) => {
-      if (
-        !addResourceBtn.contains(event.target) &&
-        !resourceForm.contains(event.target)
-      ) {
-        resourceForm.style.display = "none";
-        resourceSection.style.filter = "";
-        document.body.style.transition = "all 0.3s ease-in-out";
-      }
-    });
-  });
-}
-addResourceFormShow();
 
 // the two default resources
 const defaultResources = [
@@ -306,3 +131,151 @@ defaultResources.forEach((resource, index) => {
 });
 
 // Handle Rating change and color code
+
+// Make Dom Manager Class
+
+class DOMManager {
+  constructor(tableContent) {
+    this.tableContent = tableContent;
+    this.resourceForm = document.querySelector(".resource-form");
+    this.resourceSection = document.querySelector(".resource-section");
+    this.addResourceBtn = document.querySelector(".add-resource-btn");
+    this.addResourceBtn.addEventListener(
+      "click",
+      this.toggleResourceForm.bind(this)
+    );
+  }
+
+  // Toggle Resource form show, blur rest
+  addResourceFormShow(event) {
+    const addResourceBtn = document.querySelector(".add-resource-btn");
+
+    if (window.getComputedStyle(this.resourceForm).display === "none") {
+      this.resourceForm.style.display = "block";
+      this.resourceSection.style.filter = "blur(2px)";
+      document.body.style.transition = "all 0.3s ease-in-out";
+    } else {
+      this.resourceForm.style.display = "none";
+      this.resourceSection.style.filter = "";
+      document.body.style.transition = "all 0.3s ease-in-out";
+    }
+    // ability to hide display if click anywhere on body but resource form
+    document.addEventListener("click", (event) => {
+      if (
+        !addResourceBtn.contains(event.target) &&
+        !this.resourceForm.contains(event.target)
+      ) {
+        this.resourceForm.style.display = "none";
+        this.resourceSection.style.filter = "";
+        document.body.style.transition = "all 0.3s ease-in-out";
+      }
+    });
+  }
+
+  // Reset other sorts except column clicked
+  resetButtons(event) {
+    // tableHeaderButtons is not defined, assuming it's a global variable
+    [...tableHeaderButtons].map((button) => {
+      if (button !== event.target) {
+        button.removeAttribute("data-dir");
+      }
+    });
+  }
+
+  // Load library from local storage on page load
+  loadLibraryFromLocalStorage() {
+    const storedLibrary = localStorage.getItem("myLibrary");
+    if (storedLibrary) {
+      myLibrary = JSON.parse(storedLibrary);
+      this.getTableContent(myLibrary);
+    } else {
+      myLibrary = [];
+    }
+  }
+
+  // Add Event listener for perform the new submission for row creation
+  addSubmitListener() {
+    const submitButton = document.querySelector('button[type="submit"]');
+
+    submitButton.addEventListener("click", (e) => {
+      e.preventDefault();
+
+      // get form values
+      const resource = document.getElementById("resource-input").value;
+      const category = document.getElementById("category-input").value;
+      const status = document.getElementById("status-input").value;
+      const startDate = document.getElementById("start-date-input").value;
+      const endDate = document.getElementById("end-date-input").value;
+      const notes = (document.getElementById("rating-input").value = "");
+    });
+
+    // Get data and save to variable for referencing
+    const getTableContent = (data) => {
+      data.forEach((resource, index) => {
+        const newRow = createNewRow(resource, index);
+        tableContent.append(newRow);
+      });
+    };
+
+    //   Sorts by Column
+    const sortData = (data, param, direction = "asc") => {
+      tableContent.textContent = "";
+      const sortedData = [...data].sort((a, b) => {
+        let aValue, bValue;
+        switch (param) {
+          case "resource-rating":
+            aValue = Number(a.rating);
+            bValue = Number(b.rating);
+            break;
+          case "resource-name":
+            aValue = a.resource.toLowerCase();
+            bValue = b.resource.toLowerCase();
+            break;
+          case "resource-category":
+            aValue = a.category.toLowerCase();
+            bValue = b.category.toLowerCase();
+            break;
+          case "resource-status":
+            aValue = a.status.toLowerCase();
+            bValue = b.status.toLowerCase();
+            break;
+          case "resource-start-date":
+            aValue = new Date(a.startDate);
+            bValue = new Date(b.startDate);
+            break;
+          case "resource-end-date":
+            aValue = new Date(a.endDate);
+            bValue = new Date(b.endDate);
+            break;
+          case "resource-notes":
+            aValue = a.notes.toLowerCase();
+            bValue = b.notes.toLowerCase();
+            break;
+          case "resource-options":
+
+          document.getElementById("rating-input").value = "";
+            aValue = null;
+            bValue = null;
+            break;
+          default:
+            aValue = null;
+            bValue = null;
+            break;
+        }
+        if (aValue === null || bValue === null) return 0;
+        return direction === "asc"
+          ? aValue < bValue
+            ? -1
+            : aValue > bValue
+            ? 1
+            : 0
+          : bValue < aValue
+          ? -1
+          : bValue > aValue
+          ? 1
+          : 0;
+      });
+      getTableContent(sortedData);
+    };
+  }
+}
